9729bcd9fbe439ff1450094b710067ddf9f71448
PacCol <pacome.colinet1@gmail.com>
2020-06-24 16:58:27 +0200

    update

--- jpeg/jpeg.h
+++ jpeg/jpeg.h
@@ -2,8 +2,8 @@ int readJpegImage(char* fileName);
 
 class jpegImage {
 public:
-    explicit jpegImage(char* fileName);
-    int width;
-    int height;
-    int pixelSize;
+  explicit jpegImage(char* fileName);
+  int width;
+  int height;
+  int pixelSize;
 };
