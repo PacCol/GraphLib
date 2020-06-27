11e6f40358e148481ca91e500662ff8a94685b60
PacCol <pacome.colinet1@gmail.com>
2020-06-27 11:24:02 +0200

    update

--- main.cc
+++ main.cc
@@ -10,7 +10,7 @@ int main() {
   std::cout << "Image Pixel Size : " << myImage.pixelSize << "\n";
 
   std::cout << "Image Pixel : X = 3, Y = 6 : ";
-  std::vector<uint8_t> scannedPixel = myImage.getRgbColor(3, 7);
+  std::vector<uint8_t> scannedPixel = myImage.getRgbColor(1023, 7);
   std::cout << "R -> " << +scannedPixel[0] << "\n";
   std::cout << "                             G -> " << +scannedPixel[1] << "\n";
   std::cout << "                             B -> " << +scannedPixel[2] << "\n";
