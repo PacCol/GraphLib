04c7011b3c9cfc1ccecaeed7c6299dc00b60b3eb
PacCol <pacome.colinet1@gmail.com>
2020-07-27 14:06:33 +0200

    update

--- src/detector/detectHumans.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include "detector.h"
-
-// We create a function to detect human eyes
-std::vector<std::vector<uint8_t>> faceDetector::detectHumanEyes() {
-
-  return pixels;
-}
--- src/jpeg/reduceNoise.cc
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-#include "jpeg.h"
-
-// We create a function to reduce the noise of the image
-void jpegImage::reduceNoise(uint8_t strength) {
-
-}
