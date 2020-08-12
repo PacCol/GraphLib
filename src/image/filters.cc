2f00b43b88f9e323f7570bf364c7f80959b38e40
PacCol <pacome.colinet1@gmail.com>
2020-08-12 15:02:46 +0200

    update

--- src/image/filters.cc
+++ src/image/filters.cc
@@ -7,20 +7,10 @@
 void Image::applyMedianFilter() {
 
   // We create a vector to save the new image
-  std::vector<std::vector<uint8_t>> newPixels;
-  // We reserve the output height
-  newPixels.reserve(height);
-
-  // We are computing the row stride
-  int rowStride = width * pixelSize;
+  std::vector<std::vector<uint8_t>> newPixels = pixels;
 
   // For each line of the image...
   for(unsigned int i = 1; i < height - 1; i++) {
-
-    // We create a vector to save the new line
-    std::vector<uint8_t> newLine;
-    // We reserve the row stride
-    newLine.reserve(rowStride);
 
     // For each pixel of this line...
     for(unsigned int j = 1; j < width - 1; j++) {
@@ -28,79 +18,33 @@ void Image::applyMedianFilter() {
       // For each value of this pixel...
       for(unsigned int k = 0; k < pixelSize; k++) {
 
-        std::vector<uint8_t> medianPixels(9);
+        std::vector<uint8_t> medianPixels;
+        medianPixels.reserve(9);
 
-        medianPixels.push_back(pixels[i][j * pixelSize + k]);
+        medianPixels.push_back(pixels[i - 1][(j -1) * pixelSize + k]);
+        medianPixels.push_back(pixels[i][(j - 1) * pixelSize + k]);
+        medianPixels.push_back(pixels[i + 1][(j - 1) * pixelSize + k]);
+
         medianPixels.push_back(pixels[i - 1][j * pixelSize + k]);
+        medianPixels.push_back(pixels[i][j * pixelSize + k]);
+        medianPixels.push_back(pixels[i + 1][j * pixelSize + k]);
+
         medianPixels.push_back(pixels[i - 1][(j + 1) * pixelSize + k]);
         medianPixels.push_back(pixels[i][(j + 1) * pixelSize + k]);
         medianPixels.push_back(pixels[i + 1][(j + 1) * pixelSize + k]);
-        medianPixels.push_back(pixels[i + 1][j * pixelSize + k]);
-        medianPixels.push_back(pixels[i + 1][(j - 1) * pixelSize + k]);
-        medianPixels.push_back(pixels[i][(j - 1) * pixelSize + k]);
-        medianPixels.push_back(pixels[i - 1][(j - 1) * pixelSize + k]);
 
         std::sort(medianPixels.begin(), medianPixels.end());
 
         uint8_t median = medianPixels[4];
 
-        newLine.push_back(median);
+        newPixels[i][j * pixelSize + k] = median;
       }
     }
-    // We push the new line into the newPixels vector
-    newPixels.push_back(newLine);
   }
 
   // We update the image
   pixels = newPixels;
 }
-
-/*
-// ###
-// #o#
-// ###
-medianPixels.push_back(pixels[i][j * pixelSize + k]);
-
-// #o#
-// ###
-// ###
-medianPixels.push_back(pixels[i - 1][j * pixelSize + k]);
-
-// ##o
-// ###
-// ###
-medianPixels.push_back(pixels[i - 1][(j + 1) * pixelSize + k]);
-
-// ###
-// ##o
-// ###
-medianPixels.push_back(pixels[i][(j + 1) * pixelSize + k]);
-
-// ###
-// ###
-// ##o
-medianPixels.push_back(pixels[i + 1][(j + 1) * pixelSize + k]);
-
-// ###
-// ###
-// #o#
-medianPixels.push_back(pixels[i + 1][j * pixelSize + k]);
-
-// ###
-// ###
-// o##
-medianPixels.push_back(pixels[i + 1][(j - 1) * pixelSize + k]);
-
-// ###
-// o##
-// ###
-medianPixels.push_back(pixels[i][(j - 1) * pixelSize + k]);
-
-// o##
-// ###
-// ###
-medianPixels.push_back(pixels[i - 1][(j - 1) * pixelSize + k]);
-*/
 
 // We create a function to show the edge of the objects of the image
 void Image::applyCannyFilter() {
